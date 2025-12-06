f86cbad23face463b798be9e8cbc34f707be83ff
Kalidou GUISSE <kalidouguisse16@gmail.com>
2025-12-06 21:35:51 +0000

    ajoute de l'origine https://frontend-gestion-salaire.vercel.app

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Autoriser ton frontend
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // <-- URLs de ton frontend React (incluant le port alternatif)
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://frontend-gestion-salaire.vercel.app'], // <-- URLs de ton frontend React (incluant le port alternatif)
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // <-- autoriser toutes les méthodes que tu utilises
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // <-- important pour les cookies/tokens JWT
